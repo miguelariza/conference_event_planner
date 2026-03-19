dfab725beaa88d528a035b575428f5663fb0077e
miguelariza <miguelangel.ariza@gmail.com>
2026-03-19 10:54:07 -0400

    new photos added and write code for add-ons section

--- src/avSlice.js
+++ src/avSlice.js
@@ -3,16 +3,51 @@ import { createSlice } from "@reduxjs/toolkit";
 export const avSlice = createSlice({
   name: "av",
   initialState: [
-    
+    {
+        img: '../../public/squirrel_photos-speakers-4109274.jpg',
+        name: "Speakers",
+        cost: 35,
+        quantity: 0,
+    },
+    {
+        img: '../../public/lograstudio-public-speaking-3926344_1280.jpg',
+        name: "Microphones",
+        cost: 45,
+        quantity: 0,
+    },
+    {
+        img: '../../public/mnplatypus-whiteboard-2903269_1280.png',
+        name: "Whiteboards",
+        cost: 80,
+        quantity: 0,
+    },
+    {
+        img: '../../public/publicdomainpictures-business-20031.jpg',
+        name: "Projectors",
+        cost: 200,
+        quantity: 0,
+    },
+    {
+        img: '../../public/geralt-signpost-235079.jpg',
+        name: "Signage",
+        cost: 80,
+        quantity: 0,
+    },
   ],
 
 
   reducers: {
     incrementAvQuantity: (state, action) => {
-      
+        const item = state[action.payload];
+        if(item) {
+            item.quantity++;
+        }
     },
     decrementAvQuantity: (state, action) => {
-     
+        const item = state[action.payload];
+        if(item && item.quantity > 0) {
+            item.quantity--;
+        }
     },
   },
 });
